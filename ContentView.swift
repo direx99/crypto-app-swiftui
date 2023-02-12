0822c4c6ddf7556505042a146e14783154842c52
Dinith <codse202f060@student.nibm.lk>
2023-02-12 20:52:52 +0530

    first commit

---
+++ ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Crypto App
+//
+//  Created by Dinith Hasaranga on 2023-02-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Crypto_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_AppApp.swift
+//  Crypto App
+//
+//  Created by Dinith Hasaranga on 2023-02-12.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
